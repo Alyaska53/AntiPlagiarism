a9e38e31ee950e17003a05a4a094f43ef8d543c8
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-05-25 23:32:37 +0300

    add functions isSeparator, length, canonization, arrayCreation, shingleRepetition

--- antiPlagiarism.cpp
+++ antiPlagiarism.cpp
@@ -26,3 +26,132 @@
 ! их генерировать. Это надо обсудить...
 
 ****************************************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+bool isSeparator(char c);
+int length(string text);
+int canonization(string text, char textArray[]);
+void arrayCreation(char text[], string arrayOfWords[]);
+int shingleRepetition(string textWordsArray[], string fragmentWordsArray[], int textWordsCount, int fragmentWordsCount);
+char separator[]=" ,.!?";
+
+int main()
+{
+	string text = "Hello  . My name    is Liza. I   am   23 years old. I like ??? programming! This is my first c++ project.";
+	string fragment = "Hello  . My name    is . I   am   23 years . I like ??? shopping! This is my first java project.";
+	
+	int textLength = length(text);
+	int fragmentLength = length(fragment);
+	
+	char canonizedText[textLength];
+	char canonizedFragment[fragmentLength];
+	
+	int textWordsCount = canonization(text, canonizedText);
+	int fragmentWordsCount = canonization(fragment, canonizedFragment);
+	
+	string textWordsArray[textWordsCount];
+	string fragmentWordsArray[fragmentWordsCount];
+	
+	arrayCreation(canonizedText, textWordsArray);
+	arrayCreation(canonizedFragment, fragmentWordsArray);
+	
+	int matches = shingleRepetition(textWordsArray, fragmentWordsArray, textWordsCount, fragmentWordsCount);
+	
+	cout << matches;
+	
+	return 0;
+}
+
+bool isSeparator(char c)
+{
+	
+    for(int i = 0; separator[i] != '\0'; i++) {
+    	if(c == separator[i]) {
+    		return true;
+    	}
+    }
+    
+	return false;
+}
+
+int length(string text)
+{
+	int counter = 0;
+	
+	for(int i = 0; text[i] != '\0'; i++) {
+		counter++;
+	}
+	
+	return counter;
+}
+
+int canonization(string text, char textArray[])
+{
+	int index = 0;
+	int wordsCounter = 0;
+	
+	for (int i = 0; text[i] != '\0'; i++) {
+    	
+        if (!isSeparator(text[i])) {
+	        textArray[index] = text[i];
+	        index++;
+	    
+	        if (isSeparator(text[i + 1]) or text[i + 1] == '\0') {
+	            textArray[index] = ' ';
+	            index++;
+	            wordsCounter++;
+        	}	
+        }
+    }
+    
+	for(int i = index; text[i] != '\0'; i++) {
+		textArray[i] = '\0';
+	}
+	
+	return wordsCounter;
+	
+}
+
+void arrayCreation(char text[], string arrayOfWords[])
+{
+	string word = "";
+	int index = 0;
+	
+	for(int i = 0; text[i] != '\0'; i++){
+		
+		if(text[i] != ' '){
+			word+=text[i];
+		}
+		else {
+			arrayOfWords[index] = word;
+				index++;
+				word = "";
+		}	
+	}
+}
+
+int shingleRepetition(string textWordsArray[], string fragmentWordsArray[], int textWordsCount, int fragmentWordsCount)
+{
+	int matches = 0;
+	
+	for(int i = 0; i <= (fragmentWordsCount-2); i++){
+		
+		for(int j = 0; j <= (textWordsCount-2); j++){
+			
+			if((fragmentWordsArray[i] == textWordsArray[j]) 
+			and(fragmentWordsArray[i+1] == textWordsArray[j+1])
+			and(fragmentWordsArray[i+2] == textWordsArray[j+2])){
+				matches++;
+			}
+		}
+	}
+	
+	return matches;
+	
+}
+
+
+
