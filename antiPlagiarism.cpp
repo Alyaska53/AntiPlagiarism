029c9ad00bb7d5c3ea42e6fcacaf9fb184379d56
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-05-26 17:10:09 +0300

    modify functions canonize, getCoincidencesNumber

--- antiPlagiarism.cpp
+++ antiPlagiarism.cpp
@@ -35,15 +35,15 @@ using namespace std;
 double antiPlagiarism(string text, string fragment);
 bool isSeparator(char c);
 int getLength(string text);
-string canonize(string text, int textLength);
+void canonize(string text, char canonizedText[]);
 int getWordsNumber(string canonizedText);
 void fillArray(string textWordsArray[], string canonizedText);
 int getCoincidencesNumber(string textWordsArray[], string fragmentWordsArray[], int textWordsNumber, int fragmentWordsNumber);
 
 int main()
 {
-	string text = "I'm going to the shop to buy an apples";
-	string fragment = "I'm going to the shop to buy a bananas";
+	string text = "London is the capital of Great Britain, its political, economic and cultural centre. It's one of the largest cities in the world. Its population is more than million people. London is situated on the river Thames. The city is very old and beautiful. It was founded more than two thousand years ago. Traditionally London is divided into several parts: the City, the West End, the East End and Westminster. The City is the oldest part of London, its financial and business centre. The heart of the City is the Stock Exchange. Westminster is the most important part of the capital. It's the administrative centre. The Houses of Parliament, the seat of the British Government, are there. It's a very beautiful building with two towers and a very big clock called Big Ben. Big Ben is really the bell which strikes every quarter of an hour. Opposite the Houses of Parliament is Westminster Abbey. It's a very beautiful church built over 900 years ago. The tombs of many great statesmen, scientists and writers are there.";
+	string fragment = "Minsk is the capital of Great Britain, its political, economic and cultural centre. It's one of the largest cities in the world. Its population is more than million people. London is situated on the river Thames. The city is very old and beautiful. It was founded more than two thousand years ago. Traditionally London is divided into several parts: the City, the West End, the East End and Westminster. The City is the oldest part of London, its financial and business centre. The heart of the City is the Stock Exchange. Westminster is the most important part of the capital. It's the administrative centre. The Houses of Parliament, the seat of the British Government, are there. It's a very beautiful building with two towers and a very big clock called Big Ben. Hello World!";
 
 	cout << "Result: " << antiPlagiarism(text, fragment);
 
@@ -54,9 +54,12 @@ double antiPlagiarism(string text, string fragment)
 {
 	const int TEXT_LENGTH = getLength(text);
 	const int FRAGMENT_LENGTH = getLength(fragment);
+	
+	char canonizedText[TEXT_LENGTH];
+	char canonizedFragment[FRAGMENT_LENGTH];
 
-	string canonizedText = canonize(text, TEXT_LENGTH);
-	string canonizedFragment = canonize(fragment, FRAGMENT_LENGTH);
+	canonize(text, canonizedText);
+	canonize(fragment, canonizedFragment);
 
 	int textWordsNumber = getWordsNumber(canonizedText);
 	int fragmentWordsNumber = getWordsNumber(canonizedFragment);
@@ -74,7 +77,7 @@ double antiPlagiarism(string text, string fragment)
 
 bool isSeparator(char c)
 {
-	char separator[] = " ~`!@#$%^&*()-_=+,./{[]}}?";
+	char separator[] = " ~`!@#$%^&*()-_=+,./{[]}}?\n";
 
 	for (int i = 0; separator[i] != '\0'; i++) {
 		if (c == separator[i]) {
@@ -96,24 +99,23 @@ int getLength(string text)
 	return counter;
 }
 
-string canonize(string text, int textLength)
+void canonize(string text, char canonizedText[])
 {
-	string canonizedText = "";
 	int index = 0;
 	
 	for (int i = 0; text[i] != '\0'; i++) {
 		if (!isSeparator(text[i])) {
-			canonizedText += text[i];
+			canonizedText[index] = text[i];
 			index++;
 
 			if (isSeparator(text[i + 1]) or text[i + 1] == '\0') {
-				canonizedText += ' ';
+				canonizedText[index] = ' ';
 				index++;
 			}
 		}
 	}
 	
-	return canonizedText;
+	canonizedText[index] = '\0';
 }
 
 int getWordsNumber(string canonizedText)
@@ -155,8 +157,10 @@ int getCoincidencesNumber(string textWordsArray[], string fragmentWordsArray[], 
 			if ((fragmentWordsArray[i] == textWordsArray[j]) and
 				(fragmentWordsArray[i + 1] == textWordsArray[j + 1]) and
 				(fragmentWordsArray[i + 2] == textWordsArray[j + 2])) {
-
+					
 				coincidences++;
+				
+				break;
 			}
 		}
 	}
