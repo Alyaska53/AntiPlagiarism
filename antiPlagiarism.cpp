9f65597354fac97b1f52b0ed353cbed90408c261
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-06-14 11:48:24 +0300

    Change AntiPlagiarism.cpp

--- antiPlagiarism.cpp
+++ antiPlagiarism.cpp
@@ -5,20 +5,26 @@
 using namespace std;
 
 double antiPlagiarism(string text, string fragment);
+int getLength(string text);
 bool isSeparator(char c);
 bool isNumber(char c);
 bool isSmallWord(string word);
 bool isWrongWord(string word);
-void cutSeparators(string text, char canonizedString[]);
-void cutWrongWords(char canonizedString[]);
-int getLength(string text);
-void toLowerCase(char canonizedString[]) ;
-void canonize(string text, char canonizedString[]);
-int getWordsNumber(char canonizedString[]);
-void fillArray(string textWordsArray[], char canonizedString[]);
+void cutSeparators(string text, char canonizedText[]);
+void cutWrongWords(char canonizedText[]);
+void toLowerCase(char canonizedText[]) ;
+void canonize(string text, char canonizedText[]);
+int getWordsNumber(char canonizedText[]);
+void fillArray(string textWordsArray[], char canonizedText[]);
+long RSHash(string str);
+long getShingleHash(string textWordsArray[], int shingleLength, int startIndex);
+void getShinglesHashsArray(string textWordsArray[], int textShinglesNumber, int shingleLength, long shinglesHashsArray[]); 
+void mergeSort(long unsortedArray[], int arrayLength);
+void merge(long unsortedArray[], long leftArray[], int leftArrayLength, long rightArray[], int rightArrayLength);
+bool binarySearch(long requiredHash, long sortedHashsArray[], int arrayLength);
 double getMatchesPercentage(string textWordsArray[], string fragmentWordsArray[], int textWordsNumber, int fragmentWordsNumber);
-int getWordsMatch(int shingleLength, int i, int j, string textWordsArray[], string fragmentWordsArray[]);
 double getPercentage(int coincidences, int fragmentWordsNumber);
+
 
 const int CHARACTER_A_ASCII = 65;
 const int CHARACTER_Z_ASCII = 90;
@@ -29,11 +35,8 @@ const string EMPTY_STRING = "";
 
 int main()
 {
-	//string text = "            ....London capital            Great ............ The Britain, the the the its political, economic                cultural centre. It's  largest cities in the world. Its population is more than million people. London is situated on the river Thames. The city is very old and beautiful. It was founded more than two thousand years ago. Traditionally London is divided into several parts:  City, the West End, the East End and Westminster. The City is the oldest part of London, its financial and business centre. The heart  City  Stock Exchange. Westminster is the most important part  capital. It's administrative centre. The Houses of Parliament, the seat of the British Government, are there. It's a very beautiful building with two towers and a very big clock called Big Ben. Big Ben is really the bell which strikes every quarter of an hour. Opposite the Houses of Parliament is Westminster Abbey. It's a very beautiful church built over 900 years ago. The tombs of many great statesmen, scientists and writers are there.       ";
-	//string fragment = "         Minsk  capital  Belarus, its political, economic and cultural centre. It's one  largest cities in the world. Its population is more than million people. London is situated on the river Thames. The city is very old and beautiful. It was founded more than two thousand years ago. Traditionally London is divided into several parts: the City, the West End,  East End and Westminster.  City   oldest part of London, its financial and business centre. The heart of the City is the Stock Exchange. Westminster is the most important part of the capital. It's the administrative centre. Houses of Parliament, the seat of the British Government, are there. It's a very beautiful building with two towers and a very big clock called Big Ben. Hello World!           ";
- 	
- 	string text = "This is a the table";
- 	string fragment = "This is a the table Those is a the apple";
+	string text = " .          a    ....London capital            Great ............ The Britain, the the the its political, economic                cultural centre. It's  largest cities in the world. Its population is more than million people. London is situated on the river Thames. The city is very old and beautiful. It was founded more than two thousand years ago. Traditionally London is divided into several parts:  City, the West End, the East End and Westminster. The City is the oldest part of London, its financial and business centre. The heart  City  Stock Exchange. Westminster is the most important part  capital. It's administrative centre. The Houses of Parliament, the seat of the British Government, are there. It's a very beautiful building with two towers and a very big clock called Big Ben. Big Ben is really the bell which strikes every quarter of an hour. Opposite the Houses of Parliament is Westminster Abbey. It's a very beautiful church built over 900 years ago. The tombs of many great statesmen, scientists and writers are there.       ";
+	string fragment = "         Minsk  capital  Belarus, its political, economic and cultural centre. It's one  largest cities in the world. Its population is more than million people. London is situated on the river Thames. The city is very old and beautiful. It was founded more than two thousand years ago. Traditionally London is divided into several parts: the City, the West End,  East End and Westminster.  City   oldest part of London, its financial and business centre. The heart of the City is the Stock Exchange. Westminster is the most important part of the capital. It's the administrative centre. Houses of Parliament, the seat of the British Government, are there. It's a very beautiful building with two towers and a very big clock called Big Ben. Hello World!           ";
  	
 	cout << "Plagiarism percentage: " << antiPlagiarism(text, fragment) << "%";
 
@@ -54,9 +57,6 @@ double antiPlagiarism(string text, string fragment)
 
 	canonize(text, canonizedText);
 	canonize(fragment, canonizedFragment);
-	
-	cout << canonizedText << " AP" << endl;
-	cout << canonizedFragment << " AP" << endl;
 
 	int textWordsNumber = getWordsNumber(canonizedText);
 	int fragmentWordsNumber = getWordsNumber(canonizedFragment);
@@ -72,6 +72,17 @@ double antiPlagiarism(string text, string fragment)
 	fillArray(fragmentWordsArray, canonizedFragment);
 	
 	return getMatchesPercentage(textWordsArray, fragmentWordsArray, textWordsNumber, fragmentWordsNumber);
+}
+
+int getLength(string text)
+{
+	int counter = 0;
+
+	for (int i = 0; text[i] != TERMINAL_NULL; i++) {
+		counter++;
+	}
+
+	return counter;
 }
 
 bool isSeparator(char c)
@@ -127,68 +138,53 @@ bool isWrongWord(string word)
 	return false;
 }
 
-void toLowerCase(char canonizedString[]) 
+void toLowerCase(char canonizedText[]) 
 {
-	for (int i = 0; canonizedString[i] != TERMINAL_NULL; i++) {
-		if ((canonizedString[i] >= CHARACTER_A_ASCII) and (canonizedString[i] <= CHARACTER_Z_ASCII)) {
-			canonizedString[i] += LOWERCASE_UPPERCASE_INTERVAL;
+	for (int i = 0; canonizedText[i] != TERMINAL_NULL; i++) {
+		if ((canonizedText[i] >= CHARACTER_A_ASCII) and (canonizedText[i] <= CHARACTER_Z_ASCII)) {
+			canonizedText[i] += LOWERCASE_UPPERCASE_INTERVAL;
 		}
 	}
-	
-	cout << canonizedString << " lowerCase" << endl;
 }
 
-int getLength(string text)
-{
-	int counter = 0;
-
-	for (int i = 0; text[i] != TERMINAL_NULL; i++) {
-		counter++;
-	}
-
-	return counter;
-}
-
-void cutSeparators(string text, char canonizedString[])
+void cutSeparators(string text, char canonizedText[])
 {
 	int index = 0;
 	
 	for (int i = 0; text[i] != TERMINAL_NULL; i++) {
 		if (!isSeparator(text[i]) and !isNumber(text[i])) {
-				canonizedString[index] = text[i];
+				canonizedText[index] = text[i];
 				index++;
 			
 			if (isSeparator(text[i + 1]) or isNumber(text[i + 1])) {
-				canonizedString[index] = SPACE;
+				canonizedText[index] = SPACE;
 				index++;
 			}
 		}
 	}
 	
-	cout << canonizedString << " cutSep 1" << endl;
-	canonizedString[index] = TERMINAL_NULL;
-	cout << canonizedString << " cutSep 2" << endl;
+	canonizedText[index] = TERMINAL_NULL;
 }
 
-void cutWrongWords(char canonizedString[])
+void cutWrongWords(char canonizedText[])
 {
 	int index = 0;
 	string word = EMPTY_STRING;
 	
-	for (int i = 0; canonizedString[i] != TERMINAL_NULL; i++) {
-		if (canonizedString[i] != SPACE) {
-			word += canonizedString[i];
+	for (int i = 0; canonizedText[i] != TERMINAL_NULL; i++) {
+		if (canonizedText[i] != SPACE) {
+			word += canonizedText[i];
 			
-			if (canonizedString[i + 1] == SPACE or canonizedString[i + 1] == TERMINAL_NULL) {
+			if (canonizedText[i + 1] == SPACE or canonizedText[i + 1] == TERMINAL_NULL) {
 				if (!isSmallWord(word) and !isWrongWord(word)) {
 					int wordLength = getLength(word);
 				
 					for (int i = 0; i < wordLength; i++) {
-						canonizedString[index] = word[i];
+						canonizedText[index] = word[i];
 						index++;
 					}
 					
-					canonizedString[index] = SPACE;
+					canonizedText[index] = SPACE;
 					index++;
 				}
 				
@@ -197,26 +193,22 @@ void cutWrongWords(char canonizedString[])
 		}
 	}
 	
-	cout << canonizedString << " cutWrongWords 1" << endl;
-	canonizedString[index] = TERMINAL_NULL;
-	cout << canonizedString << " cutWrongWords 2" << endl;
+	canonizedText[index] = TERMINAL_NULL;
 }
 
-void canonize(string text, char canonizedString[])
+void canonize(string text, char canonizedText[])
 {
-	cutSeparators(text, canonizedString);
-	toLowerCase(canonizedString);
-	cutWrongWords(canonizedString);
-	
-	cout << canonizedString << " canonize" << endl;
+	cutSeparators(text, canonizedText);
+	toLowerCase(canonizedText);
+	cutWrongWords(canonizedText);
 }
 
-int getWordsNumber(char canonizedString[])
+int getWordsNumber(char canonizedText[])
 {
 	int wordsCounter = 0;
 
-	for (int i = 0; canonizedString[i] != TERMINAL_NULL; i++) {
-		if (canonizedString[i] == SPACE or canonizedString[i + 1] == TERMINAL_NULL) {
+	for (int i = 0; canonizedText[i] != TERMINAL_NULL; i++) {
+		if (canonizedText[i] == SPACE or canonizedText[i + 1] == TERMINAL_NULL) {
 			wordsCounter++;
 		}
 	}
@@ -240,6 +232,121 @@ void fillArray(string textWordsArray[], char canonizedText[])
 	}
 }
 
+long RSHash(string str)  
+{  
+    int b = 378551;
+    int a = 63689;
+    long hash = 0;
+    
+    for (int i = 0; i < getLength(str); i++) {
+        hash = hash * a + (int)str[i];
+        a = a * b;
+    }
+    
+    return hash;
+}
+
+long getShingleHash(string textWordsArray[], int shingleLength, int startIndex)
+{
+    long hash = 0;
+    string shingle = EMPTY_STRING;
+    
+    for (int i = 0; i < shingleLength; i++) {
+        shingle += textWordsArray[startIndex + i];
+    }
+    
+    hash = RSHash(shingle);  
+    return hash;
+}
+
+void getShinglesHashsArray(string textWordsArray[], int textShinglesNumber, int shingleLength, long shinglesHashsArray[]) 
+{
+	for (int i = 0; i < textShinglesNumber; i++) {
+		shinglesHashsArray[i] = getShingleHash(textWordsArray, shingleLength, i);
+	}
+}
+
+void mergeSort(long unsortedArray[], int arrayLength)
+{
+	if (arrayLength == 1) {
+		return;
+	}
+	
+	int mid = arrayLength/2;
+	int leftArrayLength = mid;
+	int rightArrayLength = arrayLength - mid;
+	
+	long leftArray[leftArrayLength];
+	long rightArray[rightArrayLength];
+	
+	for (int i = 0; i < mid; i++) {
+		leftArray[i] = unsortedArray[i];
+	}
+	
+	for (int i = mid; i < arrayLength; i++) {
+		rightArray[i - mid] = unsortedArray[i];
+	}
+	
+	mergeSort(leftArray, leftArrayLength);
+	mergeSort(rightArray, rightArrayLength);
+	merge(unsortedArray, leftArray, leftArrayLength, rightArray, rightArrayLength);
+}
+
+void merge(long unsortedArray[], long leftArray[], int leftArrayLength, long rightArray[], int rightArrayLength)
+{
+	int leftIndex = 0;
+	int rightIndex = 0;
+	int resultArrayIndex = 0;
+	
+	while ((leftIndex < leftArrayLength) and (rightIndex < rightArrayLength)) {
+		if(leftArray[leftIndex] < rightArray[rightIndex]) {
+			unsortedArray[resultArrayIndex] = leftArray[leftIndex];
+			leftIndex++;
+			resultArrayIndex++;
+		}
+		
+		else {
+			unsortedArray[resultArrayIndex] = rightArray[rightIndex];
+			rightIndex++;
+			resultArrayIndex++;
+		}
+	}
+	
+	for (int l = leftIndex; l < leftArrayLength; l++) {
+		unsortedArray[resultArrayIndex++] = leftArray[l];
+	}
+	
+	for (int r = rightIndex; r < rightArrayLength; r++) {
+		unsortedArray[resultArrayIndex++] = rightArray[r];
+	}
+}
+
+bool binarySearch(long requiredHash, long sortedHashsArray[], int arrayLength)
+{
+	bool flag = false;
+	int leftBorder = 0;
+	int rightBorder = arrayLength - 1;
+	
+	while (leftBorder <= rightBorder) {
+		int mid = (leftBorder + rightBorder) / 2;
+		
+		if (sortedHashsArray[mid] < requiredHash) {
+			leftBorder = mid + 1;
+		}
+		
+		else if (sortedHashsArray[mid] > requiredHash) {
+			rightBorder = mid - 1;
+		}
+		
+		else if (sortedHashsArray[mid] == requiredHash) {
+			flag = true;
+			break;
+		}
+	}
+	
+	return flag;
+}
+
 double getMatchesPercentage(string textWordsArray[], string fragmentWordsArray[], int textWordsNumber, int fragmentWordsNumber)
 {
 	int shinglesMatch = 0;
@@ -251,33 +358,26 @@ double getMatchesPercentage(string textWordsArray[], string fragmentWordsArray[]
 	
 	int textShinglesNumber = textWordsNumber - shingleLength + 1;
 	int fragmentShinglesNumber = fragmentWordsNumber - shingleLength + 1;
-
+	
+	long textShinglesArray[textShinglesNumber];
+	long fragmentShinglesArray[fragmentShinglesNumber];
+	
+	getShinglesHashsArray(textWordsArray, textShinglesNumber, shingleLength, textShinglesArray);
+	getShinglesHashsArray(fragmentWordsArray, fragmentShinglesNumber, shingleLength, fragmentShinglesArray);
+	
+	mergeSort(textShinglesArray, textShinglesNumber);
+	
 	for (int i = 0; i < fragmentShinglesNumber; i++) {
-		for (int j = 0; j < textShinglesNumber; j++) {
-			int wordsMatch = getWordsMatch(shingleLength, i, j, textWordsArray, fragmentWordsArray);
-			
-			if (wordsMatch == shingleLength) {
-				shinglesMatch++;
-				break;
-			}		
+		bool isFound = binarySearch(fragmentShinglesArray[i], textShinglesArray, textShinglesNumber);
+		
+		if (isFound) {
+			shinglesMatch++;
 		}
 	}
 	
 	return getPercentage(shinglesMatch, fragmentShinglesNumber);		
 }
 
-int getWordsMatch(int shingleLength, int i, int j, string textWordsArray[], string fragmentWordsArray[])
-{
-	int wordsMatch = 0;
-			
-	for (int k = 0; k < shingleLength; k++) {
-		if (fragmentWordsArray[i + k] == textWordsArray[j + k]) {
-			wordsMatch++;
-		}
-	}
-	
-	return wordsMatch;
-}
 
 double getPercentage(int shinglesMatch, int fragmentShinglesNumber)
 {
